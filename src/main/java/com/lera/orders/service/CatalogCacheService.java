d05cd3c6ab44a6bd884fcf676ad29d90ee43ffdd
Lera <dudik0808@gmail.com>
2026-04-08 18:54:57 +0300

    review3 ахуенное

--- src/main/java/com/lera/orders/service/CatalogCacheService.java
+++ src/main/java/com/lera/orders/service/CatalogCacheService.java
@@ -1,82 +1,36 @@
 package com.lera.orders.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lera.orders.clients.CatalogClient;
 import com.lera.orders.dto.catalog.GetGoodsListRequest;
 import com.lera.orders.dto.catalog.GetGoodsListResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.RedisStringCommands;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class CatalogCacheService {
 
-    @Value("${cache.catalog.good.ttl}")
-    private Duration ttl;
     private final CatalogClient catalogClient;
-    private final StringRedisTemplate redisTemplate;
-    private final ObjectMapper objectMapper;
+    private final RedisService redisService;
 
     public GetGoodsListResponse getGoods(List<String> externalIds) {
-        List<GetGoodsListResponse.GoodDto> foundGoods = new ArrayList<>();
-        List<String> nonCachedGoodsIds = new ArrayList<>();
+        var cachedGoods = redisService.getGoodsFromCache(externalIds);
 
-        getFromCatalog(externalIds, foundGoods, nonCachedGoodsIds);
-
-        if (!nonCachedGoodsIds.isEmpty()) {
-            putIntoCache(nonCachedGoodsIds, foundGoods);
-        }
-
-        return new GetGoodsListResponse(foundGoods);
-    }
-
-    private void getFromCatalog(List<String> externalIds, List<GetGoodsListResponse.GoodDto> foundGoods, List<String> nonCachedGoodsIds) {
-        List<String> keys = externalIds.stream()
-                .map(id -> "catalog:good:" + id)
+        var cachedGoodsIds = cachedGoods.stream()
+                .map(GetGoodsListResponse.GoodDto::externalId)
+                .collect(Collectors.toSet());
+        var nonCachedGoodsIds = new HashSet<>(externalIds).stream()
+                .filter(id -> !cachedGoodsIds.contains(id))
                 .toList();
-        List<String> values = redisTemplate.opsForValue().multiGet(keys);
 
-        for (int i = 0; i < externalIds.size(); i++) {
-            String cached = values.get(i);
-            if (cached != null) {
-                try {
-                    foundGoods.add(objectMapper.readValue(cached, GetGoodsListResponse.GoodDto.class));
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                nonCachedGoodsIds.add(externalIds.get(i));
-            }
-        }
-    }
+        var goodsFromCatalog = catalogClient.getGoodsList(new GetGoodsListRequest(nonCachedGoodsIds)).goods();
+        redisService.saveGoodsToCache(goodsFromCatalog);
 
-    private void putIntoCache(List<String> nonCachedGoodsIds, List<GetGoodsListResponse.GoodDto> foundGoods) {
-        GetGoodsListResponse fromCatalog = catalogClient.getGoodsList(new GetGoodsListRequest(nonCachedGoodsIds));
-
-        redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
-            fromCatalog.goods().forEach(good -> {
-                try {
-                    byte[] key = ("catalog:good:" + good.externalId()).getBytes();
-                    byte[] value = objectMapper.writeValueAsBytes(good);
-                    connection.stringCommands().set(key, value,
-                            Expiration.from(ttl), RedisStringCommands.SetOption.UPSERT);
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            return null;
-        });
-
-        foundGoods.addAll(fromCatalog.goods());
+        var allGoods = Stream.concat(cachedGoods.stream(), goodsFromCatalog.stream()).toList();
+        return new GetGoodsListResponse(allGoods);
     }
 }
